d03c17cc4997751020b10010afdf2e3251f8c196
Mariusz Puto <putomariusz@gmail.com>
2026-01-14 21:43:27 +0100

    removed data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 -- Update existing users to have default password
-UPDATE users SET password = '1234' WHERE password IS NULL OR password = '';
+UPDATE users SET password = '12345' WHERE password IS NULL OR password = '';
